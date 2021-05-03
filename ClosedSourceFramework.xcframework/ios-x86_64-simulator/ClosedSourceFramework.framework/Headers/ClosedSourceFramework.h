f96cabb41890c2cb5e3f809ae0fbbd97b1d9f04f
Gerson Noboa <gerson.noboa@glia.com>
2021-05-03 18:13:00 +0300

    Initial commit

---
+++ ClosedSourceFramework.xcframework/ios-x86_64-simulator/ClosedSourceFramework.framework/Headers/ClosedSourceFramework.h
@@ -0,0 +1,18 @@
+//
+//  ClosedSourceFramework.h
+//  ClosedSourceFramework
+//
+//  Created by Gerson Noboa on 3/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ClosedSourceFramework.
+FOUNDATION_EXPORT double ClosedSourceFrameworkVersionNumber;
+
+//! Project version string for ClosedSourceFramework.
+FOUNDATION_EXPORT const unsigned char ClosedSourceFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClosedSourceFramework/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "ClosedSourceFramework",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "ClosedSourceFramework",
+            targets: ["VendoredClosedSourceFramework"])
+    ],
+    dependencies: [
+        .package(
+            name: "Alamofire",
+            url: "https://github.com/Alamofire/Alamofire.git"),
+            .upToNextMajor(from: "5.4.3")
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ClosedSourceFramework",
+            url: "https://github.com/gersonnoboa/vendored-closed-source-framework/releases/download/1.0.0/ClosedSourceFramework.xcframework.zip",
+            checksum: "c4579ca971b734b607ac92b2130ac51a744cf7e39be61c94d67252050019b377"
+        ),
+        .target(
+            name: "VendoredClosedSourceFramework",
+            dependencies: [
+                "ClosedSourceFramework",
+                "Alamofire"
+            ]
+        )
+    ]
+)
