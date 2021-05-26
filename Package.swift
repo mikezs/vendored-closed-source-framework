e34d1b685e376a2f196608e356544c7dad48c432
Mike Bignell <1911467+mikezs@users.noreply.github.com>
2021-05-26 13:44:21 +0100

    Update Package.swift to not include dependancies

--- Package.swift
+++ Package.swift
@@ -12,30 +12,16 @@ let package = Package(
             name: "ClosedSourceFramework",
             targets: ["VendoredClosedSourceFramework"])
     ],
-    dependencies:[ 
-        .package(
-            name: "Alamofire",
-            url: "https://github.com/Alamofire/Alamofire.git",
-            .upToNextMajor(from: "5.4.3")
-        ),
-        .package(
-            name: "TwilioVoice",
-            url: "https://github.com/gersonnoboa/twilio-voice-ios.git",
-            .upToNextMajor(from: "1.0.0")
-        ),
-    ],
     targets: [
         .binaryTarget(
             name: "ClosedSourceFramework",
-            url: "https://github.com/gersonnoboa/vendored-closed-source-framework/releases/download/1.0.2/ClosedSourceFramework.xcframework.zip",
+            url: "https://github.com/mikezs/vendored-closed-source-framework/releases/download/1.0.2/ClosedSourceFramework.xcframework.zip",
             checksum: "b5c23a07cc9b2c4dcc306581ef63eb94fb52e1b032634bdbe00fdea46117d18f"
         ),
         .target(
             name: "VendoredClosedSourceFramework",
             dependencies: [
-                "ClosedSourceFramework",
-                "Alamofire",
-                "TwilioVoice"
+                "ClosedSourceFramework"
             ]
         )
     ]
